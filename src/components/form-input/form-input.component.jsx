fdaec007608980ad4071602fd1cbc313d72f021e
Bruno Vinícius <bvmcardoso@gmail.com>
2023-10-22 00:48:51 -0300

    feat: added ability to call conversabot-backend

---
+++ src/components/form-input/form-input.component.jsx
@@ -0,0 +1,14 @@
+/** @format */
+import React from 'react';
+import './form-input.styles.scss';
+
+const FormInput = ({ label, value, onChange, ...otherProps }) => {
+  return (
+    <div className="form-input-container">
+      <label>{label}</label>
+      <input type="text" value={value} onChange={onChange} {...otherProps} />
+    </div>
+  );
+};
+
+export default FormInput;
--- src/routes/chat/chat.component.jsx
+++ src/routes/chat/chat.component.jsx
@@ -1,8 +1,50 @@
 /** @format */
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { ReactComponent as Plus } from '../../assets/plus.svg';
 import './chat.styles.scss';
+import Button from '../../components/button/button.component';
+import FormInput from '../../components/form-input/form-input.component';
+
 const Chat = () => {
+  const [aiSearch, setAiSearch] = useState('');
+  const [aiResult, setAiResult] = useState('');
+
+  const handleAiSearchChange = (event) => {
+    event.preventDefault();
+    setAiSearch(event.target.value);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const url = 'http://localhost:8080/api/chats/';
+    // Make a post to the backend:
+
+    try {
+      const response = await fetch(url, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ aiSearch }),
+      });
+
+      //   Parse and set the AI result from the response
+      const resultData = await response.json();
+      setAiResult(resultData.message);
+      console.log(aiResult);
+
+      if (!response.ok) {
+        const errorText = await response.text();
+        throw new Error(
+          `Failed to submit the form. Server response: ${errorText}`
+        );
+      }
+    } catch (error) {
+      console.log('Error submitting the form', error.message);
+    }
+  };
+
   return (
     <div className="chat">
       <nav className="sidebar">
@@ -18,7 +60,22 @@ const Chat = () => {
           &copy; 2023 by Bruno Vinícius. All rights reserved.
         </div>
       </nav>
-      <div className="chat-content">side 2</div>
+      <div className="chat-content">
+        <div className="chat-content__result">
+          {aiResult && <p>{aiResult}</p>}
+        </div>
+        <div className="chat-content__search">
+          <form onSubmit={handleSubmit} className="form-container">
+            <FormInput
+              label="ChatGPT Search"
+              value={aiSearch}
+              onChange={handleAiSearchChange}
+              placeholder="How can I help you today?"
+            />
+            <Button>Search</Button>
+          </form>
+        </div>
+      </div>
     </div>
   );
 };
